a3340f6ace951fa406c3d83929f2027e71e694dd
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-02-15 21:09:04 +0700

    map created and prepare pacman.gif file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.config import Config
-Config.set('graphics', 'resizable', False)
+
+Config.set("graphics", "resizable", False)
 
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
@@ -8,8 +9,9 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
 
 # Window.size = (1200,400)
-#if window size bugged use this size instead
-Window.size = (960,320)
+# if window size bugged use this size instead
+Window.size = (960, 320)
+
 
 class GamePlay(Screen):
     # ps stands for player size
@@ -18,18 +20,24 @@ class GamePlay(Screen):
     # ww stands for Window width
     ww = NumericProperty(1200)
 
-    #wh stands for window height
+    # wh stands for window height
     wh = NumericProperty(400)
 
     def on_size(self, *args):
         print("Window size:", self.width, self.height)
 
+    def on_touch_move(self, touch):
+        print("ตำแหน่งหน้าต่าง:", Window.mouse_pos)
+
+
 class Wall(Widget):
     pass
+
 
 class PacmanApp(App):
     def build(self):
         return GamePlay()
+
 
 if __name__ == "__main__":
     PacmanApp().run()
