8c306debc7d0b4431a83bb4402c934e731f4682d
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-02-15 23:03:52 +0700

    recreate map

--- main.py
+++ main.py
@@ -9,18 +9,14 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
 
 # Window.size = (1200,400)
+
 # if window size bugged use this size instead
 Window.size = (960, 320)
 
 
 class GamePlay(Screen):
-    # ps stands for player size
     ps = NumericProperty(77)
-
-    # ww stands for Window width
     ww = NumericProperty(1200)
-
-    # wh stands for window height
     wh = NumericProperty(400)
 
     def on_size(self, *args):
