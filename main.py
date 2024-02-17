8d2cf4c93c7b4ac88c606ecc31a6d4f481728666
Chotmanat Chanakul <6610110074@email.psu.ac.th>
2024-02-17 22:15:09 +0700

    pac move

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
 from player import *
+from kivy.clock import Clock
 
 Window.size = (1200,400)
 
@@ -27,16 +28,37 @@ class GamePlay(Screen):
         print("ตำแหน่งหน้าต่าง:", Window.mouse_pos)
         
     pacman = Player()
+    
+    def __init__(self, **kwargs):
+        super(GamePlay,self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
 
-
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'up':
+            self.pacman.velocity=(0,1)
+        elif keycode[1] == 'down':
+            self.pacman.velocity=(0,-1)
+        elif keycode[1] == 'left':
+            self.pacman.velocity=(-1,0)
+        elif keycode[1] == 'right':
+            self.pacman.velocity=(1,0)
+        return True
+    def update(self, dt):
+        self.pacman.move()
 class Wall(Widget):
     pass
 
 
 class PacmanApp(App):
     def build(self):
-        return GamePlay()
-
+        game = GamePlay()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
 if __name__ == "__main__":
     PacmanApp().run()
--- player.py
+++ player.py
@@ -7,3 +7,16 @@ class Player(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        
+        self.pos = Vector(*self.velocity)+self.pos
+        
+        if self.velocity == [0, 1]:
+            self.pac_img = "img/pac_up.gif"
+        elif self.velocity == [0, -1]:
+            self.pac_img = "img/pac_down.gif"
+        elif self.velocity == [-1, 0]:
+            self.pac_img = "img/pac_left.gif"
+        elif self.velocity == [1, 0]:
+            self.pac_img = "img/pac_right.gif"
