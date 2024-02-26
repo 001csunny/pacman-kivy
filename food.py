0385416aab97383ff8cfe1441bdc3d6767329694
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-02-26 18:06:42 +0700

    show food 98%

--- food.py
+++ food.py
@@ -7,6 +7,10 @@ for a0, a1, b0, b1 in passages:
     if a0==b0:
         for j in range(int(a1)+20, int(b1)-20,20):
             food.append([a0 + 38.5, j + 38.5])
+    
+    else:
+        for j in range(int(a0),int(b0),20):
+            food.append([j + 38.5, a1 + 38.5])
 
 food = food[:191]
 
