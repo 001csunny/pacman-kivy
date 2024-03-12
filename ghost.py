c21b5792475a2ee4a7ee2519be2b7aab05850427
Chotmanat Chanakul <6610110074@email.psu.ac.th>
2024-03-12 19:27:22 +0700

    finally my oh ghost

--- ghost.py
+++ ghost.py
@@ -6,62 +6,12 @@ from random import randint
 from math import *
 from food import *
 
-from player import  graph, close_list
+from player import  bound,passages,graph, close_list
 
 
 from gostBrain import dijkstra, distance, argmin
 
-bound = {}
-bound[1] = (0, 164)
-bound[2] = (77, 164) # bound[11]
-bound[3] = (1055, 164)
-bound[4] = (1123, 164)
-bound[5] = (77, 273) # bound[6]
-bound[6] = (77, 48) # bound[7]
-bound[7] = (1055, 49) # bound[10]
-bound[8] = (158, 49) # bound[12]
-bound[9] = (158, 272) # bound[13]
-bound[10] = (158, 164) # bound[14]
-bound[11] = (232, 164) # bound[15]
-bound[12] = (1055, 273) # bound[16]
-bound[13] = (233, 49) # bound[17]
-bound[14] = (233, 272) # bound[18]
-bound[15] = (435, 49) # bound[19]
-bound[16] = (435, 273) # bound[20]
-bound[17] = (435, 172) # bound[21]
-bound[18] = (362, 172) # bound[22]
-bound[19] = (565, 49) # bound[23]
-bound[20] = (565, 0) # bound[24]
-bound[21] = (565, 273) # bound[25]
-bound[22] = (565, 332) # bound[26]
-bound[23] = (745, 49) # bound[27]
-bound[24] = (745, 273) # bound[28]
-bound[25] = (981, 49) # bound[29]
-bound[26] = (981, 273) # bound[30]
-bound[27] = (852, 140) # bound[31]
-bound[28] = (981, 140) # bound[32]
 
-
-passages = [
-    bound[1] + bound[2],
-    bound[3] + bound[4],
-   
-    bound[6] + bound[5],
-    bound[6] + bound[7],
-    bound[8] + bound[9],
-    bound[10] + bound[11],
-    bound[5] + bound[12],
-    bound[7] + bound[12],
-    bound[13] + bound[14],
-    bound[15] + bound[16],
-    bound[18] + bound[17],
-    bound[20] + bound[19],
-    bound[21] + bound[22],
-    bound[23] + bound[24],
-    bound[25] + bound[28],
-    bound[28] + bound[26],
-    bound[27] + bound[28],
-]
 class Ghost(Widget):
 
     sp = StringProperty('img/ghost01.gif')
@@ -79,6 +29,8 @@ class Ghost(Widget):
     def move(self, randomly=True):
 
         last_pos = self.pos.copy()
+        moved = False
+
         print(self.pos)
         for passage in passages:
             if (passage[0] <= self.velocity_x + self.pos[0]) and \
@@ -88,6 +40,7 @@ class Ghost(Widget):
 
                 self.pos = Vector(*self.velocity)+self.pos
                 self.elan = self.velocity.copy()
+                moved = True
 
                 if self.velocity == [-1, 0]:
                     self.sp = 'img/ghost01.gif'
@@ -125,7 +78,7 @@ class Ghost(Widget):
             self.pos = [bound[1][0], (bound[1][1])]
 
         if self.pos == last_pos:
-            if (self.strat[1] == [] or randomly) and ((self.pos[0], self.pos[1]) in bound):
+            if (self.strat[1]==[] or randomly) and ((self.pos[0], self.pos[1]) in bound):
                 self.direction()
                 self.move()
 
